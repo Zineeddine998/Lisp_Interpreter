eaacf861bf6c8b86508e024d266f6b425d67fddb
Zineeddine998 <55639522+Zineeddine998@users.noreply.github.com>
2020-07-05 00:13:58 +0100

    main function

--- src/main.ts
+++ src/main.ts
@@ -62,3 +62,97 @@ function ParseListActual(elements: string[], i: number): [List, number] {
 	}
   return [result, i];
 }
+
+export function ParseList(s :  string) : List {
+	if(s.length == 0) throw new Error("Expect a valid string here");
+	s = s.trim();
+	if(s[0] !== '(') throw new Error("Debut '('  non trouve");
+	const elements = ToChars(s);
+
+	const [l, m] = ParseListActual(elements, 0);
+	return <List>l.items[0];
+}
+
+export function isAtom( o :Atom | List ): o is Atom {
+	return ( o as Atom).value !== undefined;
+
+}
+
+export function isList(o: Atom | List): o is List {
+	return (o as List).items !== undefined
+}
+
+
+export function first( l: List) : Atom {
+	if(l.items.length === 0) throw new Error("Ls liste est vide");
+	if(isAtom(l.items[0])){
+		return l.items[0];
+	}else{
+		throw new Error("Atom au debut de la list");
+	}
+
+}
+
+export function rest(l : List) : List {
+	let result = <List>{items : []};
+	if(l.items.length === 0) return result;
+	result.items = l.items.slice(1);
+
+	return result;
+}
+
+type basicArithmeticOp = (a: Atom , b :Atom ) => Atom
+
+interface FunMap {
+	[key : string] : basicArithmeticOp
+}
+type ArithmeticOp = (c : number, d : number) => number;
+const validate =  ( a : Atom , b : Atom , op : ArithmeticOp) : Atom => {
+	if(typeof a.value === 'number' && typeof b.value === 'number'){
+		return <Atom>{value : op(a.value, b.value)}
+
+	}
+	throw new Error('il ya un terme invalide dans l operation');
+};
+
+const _add = (a : Atom , b :Atom ) : Atom => {
+	return validate(a, b , (c:number, d: number) => c + d);
+};
+const _sub = (a :Atom , b : Atom ): Atom => {
+	return validate(a, b, (c : number, d : number) => c -d );
+};
+
+const _mul = (a: Atom, b: Atom): Atom => {
+	return validate(a, b, (c: number, d: number) => c * d)
+};
+const _div = (a: Atom, b: Atom): Atom => {
+	return validate(a, b, (c: number, d: number) => c / d)
+};
+
+
+
+const _builtInFunctionNames: FunMap = {
+	'+': _add, '-': _sub, '*': _mul, '/': _div
+};
+export function Eval(exp: Atom | List): Atom {
+	// if Atom, return itself
+	if (isAtom(exp)) {
+		return exp
+	} else if (isList(exp)) {
+		// if List evaluate the first
+		// then evaluate the rest
+		const f = first(exp)
+		if (f.value in _builtInFunctionNames) {
+			const r = rest(exp)
+			const evaluated = r.items.map(k => Eval(k));
+			if (evaluated.length === 0) throw Error('Not enough arguments to the operator ' + f.value);
+			let a = evaluated[0];
+			const fun = _builtInFunctionNames[f.value];
+			for (let i = 1; i < evaluated.length; i++) {
+				a = fun(a, evaluated[i])
+			}
+			return a
+		}
+	}
+	throw Error('Unknown evaluation error ' + exp)
+}
